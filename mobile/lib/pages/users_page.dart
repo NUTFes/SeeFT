bdb7d1a298fad335e5341b05951d993a466a920e
K-wakaT <hasukiiq@gmail.com>
2023-06-01 19:32:59 +0900

    ユーザ一覧画面の作成

--- mobile/lib/pages/users_page.dart
+++ mobile/lib/pages/users_page.dart
@@ -1,14 +1,87 @@
-// import 'package:seeft_mobile/configs/importer.dart';
+import 'dart:developer';
 
-// class UsersPage extends StatefulWidget {
-//   @override
-//   _UsersPageState createState() => _UsersPageState();
-// }
+import 'package:seeft_mobile/configs/importer.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
-// class _UsersPageState extends State<UsersPage> {
-//   _users() async {
-//     try {
-//       var res;
-//     } catch (e) {}
-//   }
-// }
+class UsersPage extends StatefulWidget {
+  @override
+  _UsersPageState createState() => _UsersPageState();
+}
+
+class _UsersPageState extends State<UsersPage> {
+  int usersLength = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ユーザ一覧'),
+        actions: <Widget>[],
+        // debug
+      ),
+      drawer: drawer.applicationDrawer(context),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == AsyncSnapshot.waiting()) {
+            logger.w("message");
+          }
+          if (!snapshot.hasData) {
+            const Center(child: CircularProgressIndicator());
+          }
+          var userList = snapshot.data;
+          return Container(
+            padding: const EdgeInsets.all(40.0),
+            child: Column(
+              children: <Widget>[
+                Flexible(
+                  child: ListView.builder(
+                    itemCount: usersLength,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                          height: 40,
+                          child: _manualItem(snapshot.data, index, context));
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _manualItem(var users, index, context) {
+    return Container(
+      decoration: new BoxDecoration(
+          border:
+              new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))),
+      child: ListTile(
+        title: Text(
+          users[index]["name"].toString(),
+          style: TextStyle(color: Colors.black, fontSize: 14.0),
+        ),
+        onTap: () async {},
+      ),
+    );
+  }
+
+  Future getData() async {
+    try {
+      var res = await api.getUsers();
+      usersLength = res.length;
+      return res;
+    } catch (err) {
+      logger.e('don`t response. error message: $err');
+    }
+  }
+}
