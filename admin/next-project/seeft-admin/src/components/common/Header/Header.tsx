52027f36a38b4f2f1f9ac57f08ec7e4d33715e5f
KazumaSun <satoma.2001.0920@gmail.com>
2024-03-08 00:57:39 +0000

    [fix]環境構築修正

--- admin/next-project/seeft-admin/src/components/common/Header/Header.tsx
+++ admin/next-project/seeft-admin/src/components/common/Header/Header.tsx
@@ -6,7 +6,7 @@ import { HeaderProps } from './Header.type';
 const Header = (props: HeaderProps) => {
   return (
     <>
-      <div className='z-10 flex h-20 w-full bg-surface-2' >
+      <div className='fixed z-10 flex h-20 w-full bg-surface-2' >
         <div className='w-24 md:w-10'>
           <Image src='/title.svg' alt='logo' width={150} height={40} className='h-fix w-fix' />
         </div>
