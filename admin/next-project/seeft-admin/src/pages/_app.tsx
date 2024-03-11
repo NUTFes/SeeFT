fc09a867951279da97534b051752e117f1b20324
KazumaSun <satoma.2001.0920@gmail.com>
2024-03-11 10:00:49 +0000

    [fix]レイアウト修正

---
+++ admin/next-project/seeft-admin/src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { RecoilRoot } from 'recoil';
+
+import MainLayout from '@components/layout/MainLayout';
+
+import 'tailwindcss/tailwind.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </RecoilRoot>
+  );
+}
+
+export default MyApp;
--- admin/next-project/seeft-admin/src/pages/index.tsx
+++ admin/next-project/seeft-admin/src/pages/index.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import MainLayout from "@components/layout/MainLayout";
 
 export default function Home() {
   return (
-    <MainLayout>
-      <div>
-        test
-      </div>
-    </MainLayout>
+    <div>
+      test
+    </div>
   );
 }
