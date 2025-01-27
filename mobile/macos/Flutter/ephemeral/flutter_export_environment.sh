476e04265aa5dd3238972c373c2a1d1082ca22aa
kureshou <kure92350@gmail.com>
2025-01-28 05:26:42 +0900

    dockerfileのモバイル項目を削除(コメントアウト)

--- mobile/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ mobile/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/p162/fvm/versions/3.0.4"
-export "FLUTTER_APPLICATION_PATH=/Users/p162/ghq/github.com/NUTFes/SeeFT/seeft-mobile"
+export "FLUTTER_ROOT=/home/kure/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/kure/workspace/SeeFT/mobile"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
 export "PACKAGE_CONFIG=.dart_tool/package_config.json"
