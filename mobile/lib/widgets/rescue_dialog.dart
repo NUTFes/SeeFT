16104cf6d30ca0056e058f074ad0d3a8b4dd6e2e
kureshou <kure92350@gmail.com>
2025-02-09 16:15:09 +0900

    レスキューダイアログ作成

---
+++ mobile/lib/widgets/rescue_dialog.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:seeft_mobile/configs/importer.dart';
+
+openShiftDialog(BuildContext context,) async {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return SimpleDialog(
+        contentPadding: EdgeInsets.zero,
+        titlePadding: EdgeInsets.zero,
+        title: Container(
+          height: 550,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("レスキュー"), 
+              centerTitle: true,
+              actions: <Widget>[
+                IconButton(
+                  onPressed: () async {
+                    _launchURL(resURL);
+                  },
+                  icon: Icon(Icons.wrap_text),
+                  color: Colors.orangeAccent[100],
+                ),
+              ],
+            ),
+            body: Container(
+              child: TextButton(
+                child: Text("送信")
+              )
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}
