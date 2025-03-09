8e86d724eb3862e792c851b02e9ab230d7ed52b1
mike-nut <s243325@stn.nagaokaut.ac.jp>
2025-03-09 15:43:00 +0900

    [fix] rescue_controller.go

--- api/lib/internals/controller/rescue_controller.go
+++ api/lib/internals/controller/rescue_controller.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"net/http"
-
 	"github.com/NUTFes/SeeFT/api/lib/usecase"
 	"github.com/labstack/echo/v4"
 )
@@ -15,13 +14,21 @@ type RescueController interface {
 	CreateRescue(echo.Context) error
 }
 
+type RescueData struct {
+	TaskID string `json:"task_id"` // 小文字taskIDを大文字TaskIDに変更、JSONタグも修正
+}
+
 func NewRescueController(u usecase.RescueUseCase) RescueController {
 	return &rescueController{u}
 }
 
 func (r *rescueController) CreateRescue(c echo.Context) error {
-	taskID := c.FormValue("task_id")
-	err := r.u.CreateRescue(c.Request().Context(), taskID)
+	var req RescueData
+	err := c.Bind(&req)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "error")
+	}
+	err = r.u.CreateRescue(c.Request().Context(), req.TaskID) // := を = に修正、repをreqに修正
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
