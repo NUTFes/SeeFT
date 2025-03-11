a5e86ec0f29d16247ccb4b802497226e9f14061f
uchida189 <perukipoo@icloud.com>
2025-03-11 21:24:03 +0900

    [feat]CustomAppBarを作成

---
+++ mobile/lib/widgets/app_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:seeft_mobile/theme/tokens.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const CustomAppBar({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title, style: TextStyle(
+        color: AppColors.textWhite,
+        fontSize: AppFontSizes.lg,
+        fontWeight: FontWeight.bold,
+      )),
+      centerTitle: false,
+      toolbarHeight: 47,
+      backgroundColor: AppColors.main,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(47); // AppBarの標準の高さ
+}
