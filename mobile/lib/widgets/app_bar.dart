8d937806e783376252c13b7db7d49e997536c087
Takasou14 <takasou0509@ezweb.ne.jp>
2025-04-20 15:03:25 +0900

    AppBarの高さを63に修正

--- mobile/lib/widgets/app_bar.dart
+++ mobile/lib/widgets/app_bar.dart
@@ -18,7 +18,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         fontWeight: FontWeight.bold,
       )),
       centerTitle: false,
-      toolbarHeight: 47,
+      toolbarHeight: 63,
       backgroundColor: AppColors.main,
     );
   }
