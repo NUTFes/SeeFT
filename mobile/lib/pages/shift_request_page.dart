4de6c5b4df30df1fd7954663e6f13e4675177ca7
uchida189 <perukipoo@icloud.com>
2025-04-25 14:22:39 +0900

    [feat]シフト希望入力のテーブル部分完成

--- mobile/lib/pages/shift_request_page.dart
+++ mobile/lib/pages/shift_request_page.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 import 'package:seeft_mobile/pages/wait_page.dart';
 import 'package:seeft_mobile/theme/tokens.dart';
 
-final ShiftRequestTable table = ShiftRequestTable();
+// final ShiftRequestTable table = ShiftRequestTable();
 
 class ShiftRequestPage extends StatefulWidget {
   @override
@@ -16,10 +16,10 @@ class _ShiftRequestPageState extends State<ShiftRequestPage> {
   bool isLoading = true;
   // シフト希望のデータを格納する配列
   List shiftReqests = <List<bool>>[
-    List.filled(64, false),
-    List.filled(64, false),
-    List.filled(64, false),
-    List.filled(64, false),
+    List.filled(64, true),
+    List.filled(64, true),
+    List.filled(64, true),
+    List.filled(64, true),
   ];
     
   @override
@@ -28,41 +28,52 @@ class _ShiftRequestPageState extends State<ShiftRequestPage> {
     fetchData();
   }
   // シフト希望のデータを取得する関数
-  final Function fetchData = () async {
-  };
+  Future<void> fetchData() async {
+    final result = await getData(); // 非同期で配列取得
+    setState(() {
+      // shiftReqests = result;
+      isLoading = false;
+    });
+  }
   
   // シフト希望のデータを変更する関数
   void editShiftRequest(int day, int index) {
     setState(() {
-      List newShiftRequest = shiftReqests;
-      // shiftReqests = [...shiftReqests]..[day][index] = !shiftReqests[day][index];
-      newShiftRequest[day][index] = !shiftReqests[day][index];
-      shiftReqests = newShiftRequest;
+      // タップしたセルの参加可/不可を反転
+      shiftReqests[day][index] = !shiftReqests[day][index];
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    if(isLoading) {
+      return WaitPage();  // データ取得中は待機画面を表示
+    }
     
-    return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(40.0),
+    return Container(
+      padding: const EdgeInsets.all(40.0),
+      decoration: BoxDecoration(
         color: AppColors.base,
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Text("参加できない時間を選択してください", textAlign: TextAlign.start),
-              Container(
-                child: table.shiftRequestTable(shiftReqests, editShiftRequest, context)),
-            ],
-          ),
-        )
       ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Text("参加できない時間を選択してください", textAlign: TextAlign.start),
+            Container(
+              // child: table.shiftRequestTable(shiftReqests, editShiftRequest, context)),
+              child: shiftRequestTable(shiftReqests, editShiftRequest, context),
+            )
+          ],
+        ),
+      )
     );
   }
 }
 
+// データ取得のための関数
 Future getData() async {
+  // 1秒待機
+  await Future.delayed(const Duration(seconds: 1));
   // try {
   //   var userID = await store.getUserID();
   //   var res = await api.getMyShiftCurrentFirstDayRainy(userID.toString());
@@ -73,155 +84,145 @@ Future getData() async {
 }
 
 
-class ShiftRequestTable {
-  Widget shiftRequestTable(var shiftRequests, Function editShiftRequest, context) {
-  // Widget shiftRequestTable(context) {
-    // print(shifts);
-    return Table(
-        border: TableBorder.all(color: Colors.black26),
-        columnWidths: const <int, TableColumnWidth>{
-          // 0: IntrinsicColumnWidth(),
-          // 0: FlexColumnWidth(1),
-          // 1: FlexColumnWidth(1),
-          // 2: FixedColumnWidth(100.0),
-        },
-        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-        children: [
+// シフト希望のテーブルを表示するWidget
+Widget shiftRequestTable(var shiftRequests, Function editShiftRequest, context) {
+  return Table(
+      border: TableBorder.all(color: AppColors.grayLight),
+      columnWidths: const <int, TableColumnWidth>{
+        0: IntrinsicColumnWidth(),
+        // 1: FlexColumnWidth(1),
+      },
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      children: [
+        // ヘッダー
+        TableRow(
+          children: [
+            TableCell(
+              child: Container()
+            ),
+            TableCell(
+              child: Container(
+                child: Text(
+                  "準備日",
+                  style: TextStyle(
+                    color: AppColors.textBlack,
+                  ),
+                ),
+              alignment: Alignment.center,
+            )),
+            TableCell(
+              child: Container(
+                child: Text(
+                  "1日目",
+                  style: TextStyle(
+                    color: AppColors.textBlack,
+                  ),
+                ),
+              alignment: Alignment.center,
+            )),
+            TableCell(
+              child: Container(
+                child: Text(
+                  "2日目",
+                  style: TextStyle(
+                    color: AppColors.textBlack,
+                  ),
+                ),
+              alignment: Alignment.center,
+            )),
+            TableCell(
+              child: Container(
+                child: Text(
+                  "片付け日",
+                  style: TextStyle(
+                    color: AppColors.textBlack,
+                  ),
+                ),
+              alignment: Alignment.center,
+            )),
+          ]
+        ),
+        for (var index = 0; index < shiftRequests[0].length; index++)
+          // シフト希望のテーブル
           TableRow(
-            decoration: BoxDecoration(color: AppColors.base),
             children: [
               TableCell(
-                child: Container()
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    // indexから時間を計算
+                    (6 + index ~/ 4).toString() + ":" + (index * 15 % 60).toString() + (index % 4 == 0? "0":"") + "~" + (6 + (index + 1) ~/ 4).toString() + ":" + ((index + 1) * 15 % 60).toString() + ((index + 1) % 4 == 0? "0":""),
+                    style: TextStyle(color: AppColors.textBlack,),
+                  )
+              )),
+              GestureDetector(  // 準備日
+                onTap: () {
+                  editShiftRequest(0, index); // タップしたセルの参加可/不可を反転
+                  print(shiftRequests[0][index].toString());
+                },
+                child: AnimatedContainer(     // アニメーション付きのコンテナ
+                  duration: Duration(milliseconds: 200),
+                  height: 40,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: shiftRequests[0][index]? null: const Color.fromARGB(255, 196, 91, 91),
+                  ),
+                  child: shiftRequests[0][index]
+                    ? null
+                    : Icon(Icons.close_outlined, color: const Color.fromARGB(255, 119, 17, 17)),
+                ),
               ),
-              TableCell(
-                child: Container(
-                  child: Text(
-                    "準備日",
-                    style: TextStyle(
-                      color: AppColors.textBlack,
-                    ),
+              GestureDetector(  // 1日目
+                onTap: () {
+                  editShiftRequest(1, index);
+                  print(shiftRequests[1][index].toString());
+                },
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 200),
+                  height: 40,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: shiftRequests[1][index]? null: const Color.fromARGB(255, 196, 91, 91),
                   ),
-                alignment: Alignment.center,
-              )),
-              TableCell(
-                child: Container(
-                  child: Text(
-                    "1日目",
-                    style: TextStyle(
-                      color: AppColors.textBlack,
-                    ),
+                  child: shiftRequests[1][index]
+                    ? null
+                    : Icon(Icons.close_outlined, color: const Color.fromARGB(255, 119, 17, 17)),
+                ),
+              ),
+                GestureDetector(  // 2日目
+                  onTap: () {
+                    editShiftRequest(2, index);
+                    print(shiftRequests[2][index].toString());
+                  },
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 200),
+                  height: 40,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: shiftRequests[2][index]? null: const Color.fromARGB(255, 196, 91, 91),
                   ),
-                alignment: Alignment.center,
-              )),
-              TableCell(
-                child: Container(
-                  child: Text(
-                    "2日目",
-                    style: TextStyle(
-                      color: AppColors.textBlack,
-                    ),
+                  child: shiftRequests[2][index]
+                    ? null
+                    : Icon(Icons.close_outlined, color: const Color.fromARGB(255, 119, 17, 17)),
+                ),
+              ),
+              GestureDetector(  // 片付け日
+                onTap: () {
+                  editShiftRequest(3, index);
+                  print(shiftRequests[3][index].toString());
+                },
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 200),
+                  height: 40,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: shiftRequests[3][index]? null: const Color.fromARGB(255, 196, 91, 91),
                   ),
-                alignment: Alignment.center,
-              )),
-              TableCell(
-                child: Container(
-                  child: Text(
-                    "片付け日",
-                    style: TextStyle(
-                      color: AppColors.textBlack,
-                    ),
-                  ),
-                alignment: Alignment.center,
-              )),
-            ]
-          ),
-          for (var index = 0; index < shiftRequests[0].length; index++)
-            TableRow(
-                decoration: BoxDecoration(color: AppColors.base),
-                children: [
-                  TableCell(
-                      child: Container(
-                    alignment: Alignment.center,
-                    child: Text((6 + index ~/ 4).toString() + ":" + (index * 15 % 60).toString() + (index % 4 == 0? "0":"") + " ~ " + (6 + (index + 1) ~/ 4).toString() + ":" + ((index + 1) * 15 % 60).toString() + ((index + 1) % 4 == 0? "0":"")) // indexから時間を計算
-                  )),
-                  TableCell(
-                    child: GestureDetector(
-                      onTap: () {
-                        // shiftRequests[0][index] = !shiftRequests[0][index];
-                        editShiftRequest(0, index);
-                        print(shiftRequests[0][index].toString());
-                      },
-                      child: Container(
-                        alignment: Alignment.center,
-                        child: Text(shiftRequests[0][index].toString()),
-                      ),
-                    )),
-                  TableCell(
-                      child: Container(
-                    alignment: Alignment.center,
-                    child: Text(shiftRequests[1][index].toString()),
-                  )),
-                  TableCell(
-                      child: Container(
-                    alignment: Alignment.center,
-                    child: Text(shiftRequests[2][index].toString()),
-                  )),
-                  TableCell(
-                      child: Container(
-                    alignment: Alignment.center,
-                    child: Text(shiftRequests[3][index].toString()),
-                  )),
-                ]),
-          // for (var hour = 6; hour < 22; hour++)
-          //   for (var minute = 0; minute < 60; minute += 15)
-          //     TableRow(
-          //       decoration: BoxDecoration(color: AppColors.base),
-          //       children: [
-          //         TableCell(
-          //           child: Container(
-          //             alignment: Alignment.center,
-          //             child: minute == 0
-          //                 ? Text(hour.toString() + ":00 ~ " + hour.toString() + ":15")
-          //                 : minute == 45
-          //                 ? Text(hour.toString() + ":45 ~ " + (hour + 1).toString() + ":00")
-          //                 : Text(hour.toString() + ":" + minute.toString() + " ~ " + hour.toString() + ":" + (minute + 15).toString()),
-          //         )),
-          //         TableCell(
-          //             child: Container(
-          //           alignment: Alignment.center,
-          //           child: Text(shiftRequests[0][hour * 4 + minute ~/ 15].toString()),
-          //         )),
-          //         TableCell(
-          //             child: Container(
-          //           alignment: Alignment.center,
-          //           child: new Text(""),
-          //         )),
-          //         TableCell(
-          //             child: Container(
-          //           alignment: Alignment.center,
-          //           child: new Text(""),
-          //         )),
-          //         TableCell(
-          //             child: Container(
-          //           alignment: Alignment.center,
-          //           child: new Text(""),
-          //         )),
-          //     ]),
-        ]);
-  }
-}
-
-class HexColor extends Color {
-  static int _getColorFromHex(String hexColor) {
-    // logger.d(hexColor);
-    hexColor = hexColor.toUpperCase().replaceAll('0X', '');
-    // logger.d(hexColor);
-    if (hexColor.length == 6) {
-      hexColor = 'FF' + hexColor;
-    }
-    // logger.d(hexColor);
-    return int.parse(hexColor, radix: 16);
-  }
-
-  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+                  child: shiftRequests[3][index]
+                    ? null
+                    : Icon(Icons.close_outlined, color: const Color.fromARGB(255, 119, 17, 17)),
+                ),
+              ),
+            ]),
+      ]);
 }
