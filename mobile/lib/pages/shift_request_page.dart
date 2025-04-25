0aa692863c999ccb243c858f0ed88ed55eefafb8
uchida189 <perukipoo@icloud.com>
2025-04-25 12:51:30 +0900

    [feat]タップでシフト希望の変更機能の雛形できた

--- mobile/lib/pages/shift_request_page.dart
+++ mobile/lib/pages/shift_request_page.dart
@@ -13,56 +13,69 @@ class ShiftRequestPage extends StatefulWidget {
 }
 
 class _ShiftRequestPageState extends State<ShiftRequestPage> {
-
+  bool isLoading = true;
+  // シフト希望のデータを格納する配列
+  List shiftReqests = <List<bool>>[
+    List.filled(64, false),
+    List.filled(64, false),
+    List.filled(64, false),
+    List.filled(64, false),
+  ];
+    
   @override
   void initState() {
     super.initState();
+    fetchData();
+  }
+  // シフト希望のデータを取得する関数
+  final Function fetchData = () async {
+  };
+  
+  // シフト希望のデータを変更する関数
+  void editShiftRequest(int day, int index) {
+    setState(() {
+      List newShiftRequest = shiftReqests;
+      // shiftReqests = [...shiftReqests]..[day][index] = !shiftReqests[day][index];
+      newShiftRequest[day][index] = !shiftReqests[day][index];
+      shiftReqests = newShiftRequest;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-    return FutureBuilder(
-      future: getData(),
-      builder: (ctx, snapshot) {
-        if (snapshot.connectionState == AsyncSnapshot.waiting()) {
-          logger.w("message");
-        }
-        // if (!snapshot.hasData) {
-        //   // 待機画面を表示
-        //   return WaitPage();
-        // }
-        return Container(
-            padding: const EdgeInsets.all(40.0),
-            color: AppColors.base,
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Text("参加できない時間を選択してください", textAlign: TextAlign.start),
-                  Container(
-                    child: table.shiftRequestTable(context)),
-                ],
-              ),
-            ));
-      },
+    
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(40.0),
+        color: AppColors.base,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Text("参加できない時間を選択してください", textAlign: TextAlign.start),
+              Container(
+                child: table.shiftRequestTable(shiftReqests, editShiftRequest, context)),
+            ],
+          ),
+        )
+      ),
     );
   }
 }
 
 Future getData() async {
-  try {
-    var userID = await store.getUserID();
-    var res = await api.getMyShiftCurrentFirstDayRainy(userID.toString());
-    return res;
-  } catch (err) {
-    logger.e('don`t response. error message: $err');
-  }
+  // try {
+  //   var userID = await store.getUserID();
+  //   var res = await api.getMyShiftCurrentFirstDayRainy(userID.toString());
+  //   return res;
+  // } catch (err) {
+  //   logger.e('don`t response. error message: $err');
+  // }
 }
 
 
 class ShiftRequestTable {
-  // Widget shiftRequestTable(var shifts, context) {
-  Widget shiftRequestTable(context) {
+  Widget shiftRequestTable(var shiftRequests, Function editShiftRequest, context) {
+  // Widget shiftRequestTable(context) {
     // print(shifts);
     return Table(
         border: TableBorder.all(color: Colors.black26),
@@ -122,41 +135,78 @@ class ShiftRequestTable {
               )),
             ]
           ),
-          for (var hour = 6; hour < 22; hour++)
-            for (var minute = 0; minute < 60; minute += 15)
-              TableRow(
+          for (var index = 0; index < shiftRequests[0].length; index++)
+            TableRow(
                 decoration: BoxDecoration(color: AppColors.base),
                 children: [
                   TableCell(
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: minute == 0
-                          ? Text(hour.toString() + ":00 ~ " + hour.toString() + ":15")
-                          : minute == 45
-                          ? Text(hour.toString() + ":45 ~ " + (hour + 1).toString() + ":00")
-                          : Text(hour.toString() + ":" + minute.toString() + " ~ " + hour.toString() + ":" + (minute + 15).toString()),
+                      child: Container(
+                    alignment: Alignment.center,
+                    child: Text((6 + index ~/ 4).toString() + ":" + (index * 15 % 60).toString() + (index % 4 == 0? "0":"") + " ~ " + (6 + (index + 1) ~/ 4).toString() + ":" + ((index + 1) * 15 % 60).toString() + ((index + 1) % 4 == 0? "0":"")) // indexから時間を計算
+                  )),
+                  TableCell(
+                    child: GestureDetector(
+                      onTap: () {
+                        // shiftRequests[0][index] = !shiftRequests[0][index];
+                        editShiftRequest(0, index);
+                        print(shiftRequests[0][index].toString());
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Text(shiftRequests[0][index].toString()),
+                      ),
+                    )),
+                  TableCell(
+                      child: Container(
+                    alignment: Alignment.center,
+                    child: Text(shiftRequests[1][index].toString()),
                   )),
                   TableCell(
                       child: Container(
                     alignment: Alignment.center,
-                    child: new Text(""),
+                    child: Text(shiftRequests[2][index].toString()),
                   )),
                   TableCell(
                       child: Container(
                     alignment: Alignment.center,
-                    child: new Text(""),
+                    child: Text(shiftRequests[3][index].toString()),
                   )),
-                  TableCell(
-                      child: Container(
-                    alignment: Alignment.center,
-                    child: new Text(""),
-                  )),
-                  TableCell(
-                      child: Container(
-                    alignment: Alignment.center,
-                    child: new Text(""),
-                  )),
-              ]),
+                ]),
+          // for (var hour = 6; hour < 22; hour++)
+          //   for (var minute = 0; minute < 60; minute += 15)
+          //     TableRow(
+          //       decoration: BoxDecoration(color: AppColors.base),
+          //       children: [
+          //         TableCell(
+          //           child: Container(
+          //             alignment: Alignment.center,
+          //             child: minute == 0
+          //                 ? Text(hour.toString() + ":00 ~ " + hour.toString() + ":15")
+          //                 : minute == 45
+          //                 ? Text(hour.toString() + ":45 ~ " + (hour + 1).toString() + ":00")
+          //                 : Text(hour.toString() + ":" + minute.toString() + " ~ " + hour.toString() + ":" + (minute + 15).toString()),
+          //         )),
+          //         TableCell(
+          //             child: Container(
+          //           alignment: Alignment.center,
+          //           child: Text(shiftRequests[0][hour * 4 + minute ~/ 15].toString()),
+          //         )),
+          //         TableCell(
+          //             child: Container(
+          //           alignment: Alignment.center,
+          //           child: new Text(""),
+          //         )),
+          //         TableCell(
+          //             child: Container(
+          //           alignment: Alignment.center,
+          //           child: new Text(""),
+          //         )),
+          //         TableCell(
+          //             child: Container(
+          //           alignment: Alignment.center,
+          //           child: new Text(""),
+          //         )),
+          //     ]),
         ]);
   }
 }
