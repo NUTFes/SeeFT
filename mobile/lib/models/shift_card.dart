126290c4823e367a8325ffd0404badcdc5bcd4e5
uchida189 <perukipoo@icloud.com>
2025-06-16 17:41:01 +0900

    [add]シフトカードのデータモデルを作成

---
+++ mobile/lib/models/shift_card.dart
@@ -0,0 +1,102 @@
+// シフトカードのデータモデル
+class ShiftMember {
+  final String name;
+  final String grade;
+  final String bureau;
+
+  ShiftMember({
+    required this.name,
+    required this.grade,
+    required this.bureau,
+  });
+}
+class ShiftMembers {
+  final String s_time;
+  final String e_time;
+  final List<ShiftMember> members;
+
+  ShiftMembers({
+    required this.s_time,
+    required this.e_time,
+    required this.members,
+  });
+}
+
+class ShiftCardData {
+  final String taskName;
+  final String startTime;
+  final String endTime;
+  final String place;
+  final String url;
+  final List<ShiftMembers> shiftMembers;
+  final ShiftMembers beforeMembers;
+  final ShiftMembers afterMembers;
+
+  ShiftCardData({
+    required this.taskName,
+    required this.startTime,
+    required this.endTime,
+    required this.place,
+    required this.url,
+    required this.shiftMembers,
+    required this.beforeMembers,
+    required this.afterMembers,
+  });
+}
+
+
+class ShiftCardDataList {
+  final List<ShiftCardData> data;
+  ShiftCardDataList(this.data);
+  
+  // jsonからShiftCardDataListを生成するファクトリコンストラクタ
+  factory ShiftCardDataList.fromJson(List<dynamic> json) {
+    List<ShiftCardData> data = json.map((item) => ShiftCardData(
+      taskName: item['task_name'] as String,
+      startTime: item['start_time'] as String,
+      endTime: item['end_time'] as String,
+      place: item['place'] as String,
+      url: item['url'] as String,
+      shiftMembers: (item['shift_members'] as List<dynamic>)
+          .map((member) => ShiftMembers(
+                s_time: member['s_time'],
+                e_time: member['e_time'],
+                members: (member['members'] as List<dynamic>)
+                    .map((m) => ShiftMember(
+                          name: m['name'],
+                          grade: m['grade'],
+                          bureau: m['bureau'],
+                        ))
+                    .toList(),
+              ))
+          .toList(),
+      beforeMembers: ShiftMembers(
+        s_time: item['before_members']['s_time'],
+        e_time: item['before_members']['e_time'],
+        members: item['before_members']['members'] != null?
+          (item['before_members']['members'] as List<dynamic>)
+            .map((m) => ShiftMember(
+                name: m['name']?? 'データの取得に失敗しました',
+                grade: m['grade']?? '',
+                bureau: m['bureau']?? '',
+            ))
+            .toList(): 
+          [],
+      ),
+      afterMembers: ShiftMembers(
+        s_time: item['after_members']['s_time'],
+        e_time: item['after_members']['e_time'],
+        members: item['after_members']['members'] != null?
+          (item['after_members']['members'] as List<dynamic>)
+            .map((m) => ShiftMember(
+                name: m['name']?? 'データの取得に失敗しました',
+                grade: m['grade']?? '',
+                bureau: m['bureau']?? '',
+            ))
+            .toList():
+          [],
+      ),
+    )).toList();
+    return ShiftCardDataList(data);
+  }
+}
