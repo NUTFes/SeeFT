9688ec2ba94ba51645a1c04e50b3458b546c0b39
screana <ryooya0906@gmail.com>
2025-07-20 18:43:54 +0900

    [feat] タイポ修正

--- api/lib/entity/review.go
+++ api/lib/entity/review.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Review struct {
-	ID            int       `json:"id"`
-	UserID        int       `json:"user_id"`
-	TaskID        int       `json:"task_id"`
-	StaffigRating int       `json:"staffing_rating"`
-	ManualRating  int       `json:"manual_rating"`
-	Comment       string    `json:"comment"`
-	CreatedAt     time.Time `json:"created_at"`
-	UpdatedAt     time.Time `json:"updated_at"`
+	ID             int       `json:"id"`
+	UserID         int       `json:"user_id"`
+	TaskID         int       `json:"task_id"`
+	StaffingRating int       `json:"staffing_rating"`
+	ManualRating   int       `json:"manual_rating"`
+	Comment        string    `json:"comment"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
