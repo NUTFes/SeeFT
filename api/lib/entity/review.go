5a2a5e1b6d3635054826ddb71de5079d53217704
screana <ryooya0906@gmail.com>
2025-07-20 17:05:05 +0900

    [feat]レビューのエンティティ修正

--- api/lib/entity/review.go
+++ api/lib/entity/review.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Review struct {
-	ID             int       `json:"id"`
-	User_ID        int       `json:"user_id"`
-	task_id        int       `json:"task_ID"`
-	staffig_rating int       `json:"staffing_rating"`
-	manual_rating  int       `json:"manual_rating"`
-	comment        string    `json:"comment"`
-	CreatedAt      time.Time `json:"createdAt"`
-	UpdatedAt      time.Time `json:"updatedAt"`
+	ID            int       `json:"id"`
+	UserID        int       `json:"user_id"`
+	TaskID        int       `json:"task_id"`
+	StaffigRating int       `json:"staffing_rating"`
+	ManualRating  int       `json:"manual_rating"`
+	Comment       string    `json:"comment"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
 }
