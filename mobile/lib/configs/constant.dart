d0eda9c6a804dbf2fe8a61a6304e1e8d60a81f9b
uchida189 <130959298+uchida189@users.noreply.github.com>
2025-08-23 17:17:58 +0900

    [feat]環境変数からSeeFTの操作説明と全体シフトのURLを取得する機能を実装

--- mobile/lib/configs/constant.dart
+++ mobile/lib/configs/constant.dart
@@ -5,6 +5,10 @@ const _apiBaseUrl = String.fromEnvironment('API_BASE_URL', defaultValue: 'http:/
 // 環境変数から委員長の名前と電話番号を取得
 const _chairpersonName = String.fromEnvironment('CHAIRPERSON_NAME', defaultValue: '委員長');
 const _chairpersonPhoneNumber = String.fromEnvironment('CHAIRPERSON_PHONE_NUMBER', defaultValue: '00011112222');
+// 環境変数からSeeFTの操作説明のURLを取得
+const _seeftInstructionsUrl = String.fromEnvironment('SEEFT_INSTRUCTIONS_URL', defaultValue: 'https://example.com');
+// 環境変数から全体シフトのURLを取得
+const _wholeShiftUrl = String.fromEnvironment('WHOLE_SHIFT_URL', defaultValue: 'https://example.com');
 
 class Constant {
   final String appName = "SeeFT";
@@ -13,4 +17,6 @@ class Constant {
   final String apiUrl = _apiBaseUrl;
   final String chairpersonName = _chairpersonName;
   final String chairpersonPhoneNumber = _chairpersonPhoneNumber;
+  final String seeftInstructionsUrl = _seeftInstructionsUrl;
+  final String wholeShiftUrl = _wholeShiftUrl;
 }
