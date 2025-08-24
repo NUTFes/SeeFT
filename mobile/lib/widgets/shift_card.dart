c2f02478ced9b00af86053c97f418a1188f91159
uchida189 <130959298+uchida189@users.noreply.github.com>
2025-08-24 15:13:41 +0900

    [feat]シフトカードを改修。マニュアルボタンの見た目を変更。またマニュアルがない時は見た目が変わるようにした。

--- mobile/lib/widgets/shift_card.dart
+++ mobile/lib/widgets/shift_card.dart
@@ -33,13 +33,15 @@ class ShiftCard extends StatelessWidget {
       elevation: 0.5,
       shadowColor: null,
       color: AppColors.base, // 背景色を設定
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          // 時刻とマニュアルを開くボタン
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 3.0),
-            child: Row(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // 時刻とマニュアルを開くボタン
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 // 時刻の表示
@@ -61,133 +63,144 @@ class ShiftCard extends StatelessWidget {
                   ],
                 ),
                 // マニュアルを開くボタン
-                ElevatedButton(
-                  onPressed: () {
-                    // マニュアルを開くボタンの処理
-                    if (data.url.isNotEmpty) {
-                      _launchManualUrl(data.url);
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('マニュアルのURLが設定されていません')), // マニュアルのURLが空の場合のエラーメッセージ
-                      );
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.sub,
-                    foregroundColor: AppColors.textWhite,
-                    shape: const StadiumBorder(), // 角が丸い形状
-                    elevation: 0, // 影を消す
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-                  ),
-                  child: const Text(
-                    'マニュアルを開く',
-                    style: TextStyle(
-                      fontSize: AppFontSizes.xs, 
-                      color: AppColors.textWhite
-                    ),
-                  ),
-                ),
+                _manualButton(url: data.url),
               ],
             ),
-          ),
-          
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 3.0, 8.0, 8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // タスク名の表示
-                Text(
-                  data.taskName.toString(),
-                  style: const TextStyle(
-                    fontSize: AppFontSizes.md,
-                    color: AppColors.textBlack, 
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                const SizedBox(height: 6.0),
-                const Divider(
-                  height: 1,
-                  color: AppColors.grayLight, // 区切り線の色
-                ),
-                // 集合場所とトグル
-                Theme(
-                  data: Theme.of(context).copyWith(dividerColor: Colors.transparent), // 区切り線の色を透明に
-                  child: ExpansionTile(
-                    tilePadding: EdgeInsets.zero,
-                    iconColor: AppColors.textBlack, // アイコンの色を変更
-                    minTileHeight: 0, // タイルの高さを最小に
-                    // 集合場所の表示
-                    title: Row(
-                      children: [
-                        const Icon(
-                          Icons.location_on_outlined, 
-                          color: AppColors.textBlack,
-                          size: 16
-                        ),
-                        const SizedBox(width: 2.0),
-                        Text(
-                          data.place,
-                          style: const TextStyle(
-                            fontSize: AppFontSizes.sm,
-                            color: AppColors.textBlack
-                          ),
-                        ),
-                      ],
+            // タスク名の表示
+            Text(
+              data.taskName.toString(),
+              style: const TextStyle(
+                fontSize: AppFontSizes.md,
+                color: AppColors.textBlack, 
+                fontWeight: FontWeight.bold
+              ),
+              textAlign: TextAlign.left,
+            ),
+            const SizedBox(height: 6.0),
+            const Divider(
+              height: 1,
+              color: AppColors.grayLight, // 区切り線の色
+            ),
+            // 集合場所とトグル
+            Theme(
+              data: Theme.of(context).copyWith(dividerColor: Colors.transparent), // 区切り線の色を透明に
+              child: ExpansionTile(
+                tilePadding: EdgeInsets.zero,
+                iconColor: AppColors.textBlack, // アイコンの色を変更
+                minTileHeight: 0, // タイルの高さを最小に
+                // 集合場所の表示
+                title: Row(
+                  children: [
+                    const Icon(
+                      Icons.location_on_outlined, 
+                      color: AppColors.textBlack,
+                      size: 16
                     ),
-                    // トグルが展開されたときの内容
-                    children: <Widget>[
-                      _buildSection(
-                        title: '【集合場所】',
-                        content: [data.place]
+                    const SizedBox(width: 2.0),
+                    Text(
+                      data.place,
+                      style: const TextStyle(
+                        fontSize: AppFontSizes.sm,
+                        color: AppColors.textBlack
                       ),
-                      _buildSection(
-                        title: '【困った時は】',
-                        content: [
-                          '1. 近くの先輩に聞く',
-                          '2. それでも分からなかったら本部に連絡',
-                        ],
-                      ),
-                      _buildSection(
-                        title: '【担当者の一覧】',
-                        content: [
-                          for (var member in data.shiftMembers)
-                            '${member.s_time}〜${member.e_time}\n' +
-                            member.members.map((m) => '(${m.bureau}${m.grade}) ${m.name}').join(', '),
-                        ],
-                      ),
-                      _buildSection(
-                        title: '【前の時間の担当者の一覧】',
-                        content: [
-                          if (data.beforeMembers.members.isNotEmpty)
-                            '${data.beforeMembers.s_time}〜${data.beforeMembers.e_time}\n' +
-                            data.beforeMembers.members.map((m) => '(${m.bureau}${m.grade}) ${m.name}').join(', ')
-                          else
-                            '前の時間の担当者はいません',
-                        ],
-                      ),
-                      _buildSection(
-                        title: '【次の時間の担当者の一覧】',
-                        content: [
-                          if (data.afterMembers.members.isNotEmpty)
-                            '${data.afterMembers.s_time}〜${data.afterMembers.e_time}\n' +
-                            data.afterMembers.members.map((m) => '(${m.bureau}${m.grade}) ${m.name}').join(', ')
-                          else
-                            '次の時間の担当者はいません',
-                        ],
-                      ),
+                    ),
+                  ],
+                ),
+                // トグルが展開されたときの内容
+                children: <Widget>[
+                  _buildSection(
+                    title: '【集合場所】',
+                    content: [data.place]
+                  ),
+                  _buildManualSection(
+                    title: '【マニュアル】',
+                    url: data.url != '' ? data.url : null
+                  ),
+                  _buildSection(
+                    title: '【困った時は】',
+                    content: [
+                      '以下の順に対応してください。',
+                      '1. マニュアルを確認してください。',
+                      '2. 近くの人や近くの先輩に相談してください。',
+                      '3. 「緊急事対応」ページから本部に連絡してください。',
                     ],
                   ),
-                )
-              ],
+                  _buildSection(
+                    title: '【担当者の一覧】',
+                    content: [
+                      for (var member in data.shiftMembers)
+                        '${member.s_time}〜${member.e_time}\n' +
+                        member.members.map((m) => '(${m.bureau}${m.grade}) ${m.name}').join(', '),
+                    ],
+                  ),
+                  _buildSection(
+                    title: '【前の時間の担当者の一覧】',
+                    content: [
+                      if (data.beforeMembers.members.isNotEmpty)
+                        '${data.beforeMembers.s_time}〜${data.beforeMembers.e_time}\n' +
+                        data.beforeMembers.members.map((m) => '(${m.bureau}${m.grade}) ${m.name}').join(', ')
+                      else
+                        '前の時間の担当者はいません',
+                    ],
+                  ),
+                  _buildSection(
+                    title: '【次の時間の担当者の一覧】',
+                    content: [
+                      if (data.afterMembers.members.isNotEmpty)
+                        '${data.afterMembers.s_time}〜${data.afterMembers.e_time}\n' +
+                        data.afterMembers.members.map((m) => '(${m.bureau}${m.grade}) ${m.name}').join(', ')
+                      else
+                        '次の時間の担当者はいません',
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
+  // マニュアルボタン
+  Widget _manualButton({
+    required String url,
+  }) {
+    final bool isDisabled = url.isEmpty;  // マニュアルがない場合は無効化する
+    return ElevatedButton.icon(
+      onPressed: isDisabled ? null : () => _launchManualUrl(url), // マニュアルがあればタップで開く
+      icon: Icon(
+        Icons.quiz_outlined,
+        size: 16,
+        color: isDisabled ? AppColors.grayDark : AppColors.textWhite,
+      ),
+      label: Text(
+        isDisabled ? 'マニュアルなし' : 'マニュアル',
+        style: TextStyle(
+          fontSize: AppFontSizes.sm,
+          color: isDisabled ? AppColors.grayDark : AppColors.textWhite,
+        ),
+      ),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: isDisabled ? AppColors.grayLight : AppColors.link,
+        padding: const EdgeInsets.symmetric(
+          vertical: 4.0,
+          horizontal: 8.0
+        ),
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100.0),
+        ),
+        // 横幅を広げるための設定
+        // minimumSize: Size(double.infinity, 40),
+      ),
+    );
+  }
+  
   // 各セクションのUIを生成するヘルパーメソッド
-  Widget _buildSection({required String title, required List<String> content}) {
+  Widget _buildSection({
+    required String title, 
+    required List<String> content
+  }) {
     return Padding(
       padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
       child: Align(
@@ -212,6 +225,43 @@ class ShiftCard extends StatelessWidget {
                 height: 1.5 // 行間を調整
               )
             )),
+          ],
+        ),
+      ),
+    );
+  }
+  // マニュアルセクションのUIを生成するヘルパーメソッド
+  Widget _buildManualSection({
+    required String title, 
+    String? url
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: AppFontSizes.xs,
+                color: AppColors.textBlack,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            const SizedBox(height: 4.0),
+            GestureDetector(
+              onTap: () => url != null ? _launchManualUrl(url) : null,  // マニュアルが存在すればタップでマニュアルを開く
+              child: Text(
+                url ?? 'マニュアルがありません',
+                style: TextStyle(
+                  fontSize: AppFontSizes.xs,
+                  color: url != null ? AppColors.link : AppColors.textBlack,
+                  height: 1.5 // 行間を調整
+                )
+              ),
+            )
           ],
         ),
       ),
